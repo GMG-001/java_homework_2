a2879783acceab6b5e4674264bc9fceda35905c7
giorgi <giorgimegrelishvili45@gmail.com>
2021-10-26 21:33:08 +0400

    finish

--- src/main/java/FileRead.java
+++ src/main/java/FileRead.java
@@ -1,36 +1,39 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 
 public class FileRead {
+    //ლოგერრის გამოზახება
     private static final Logger logger= LoggerFactory.getLogger(FileRead.class);
 
+    //კითხულობს ფაილიდან ჩაწერილ ინფორმაციას ითვლის საშუალოს და წერს ფაილში
     public static void fileInputOutput() {
         try {
-
+            //ფაილიდან წაკითხვა
             BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/data.txt"));
             String line = bufferedReader.readLine();
-            double sum = 0;
+            int sum = 0;
             int count = 0;
             while (line != null) {
                 sum += Integer.parseInt(line);
                 line = bufferedReader.readLine();
                 count++;
             }
+            //საშუალოს გამოთვლა
+            int average = sum/count;
 
-            double average = sum/count;
-            String averageFinal = "\n"+String.valueOf(average);
-
-
+            //ფაილში ჩაწერა
             BufferedWriter br = new BufferedWriter(new FileWriter("src/main/resources/data.txt",true));
-            br.write(averageFinal);
+            br.write("\n"+ average);
             br.close();
-            logger.trace("average is: " + averageFinal );
+
+            //გამოთვლილი საშუაოს დალოგვა წარმატებით შესრულების შემთხვევაში
+            logger.trace("average is: " + average );
         } catch (Exception ex) {
+            //შეცდომის დალოგვა
             System.out.println(ex.getMessage());
             logger.error("Error during reading system input", ex);
         }
